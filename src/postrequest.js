c7de935129644a85f1ad19d1f7d77cf6f435a3ce
Abdullah Naveed <muhammad.naveed@ucdconnect.ie>
2019-01-15 12:11:57 +0000

    inital commit

---
+++ src/postrequest.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    // SUBMIT FORM
+    $("#submitForm").submit(function(event) {
+        // Prevent the form from submitting via the browser.
+        event.preventDefault();
+
+        const min = 1;
+        const max = 1000;
+        const rand = Math.floor(min + Math.random() * (max - min));
+        // this.state.id = rand;
+        fetch('http://localhost:8000/patients/addPatient', {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                id: rand,
+                name: $("#name").val(),
+                age: $("#age").val(),
+                gender : $("#gender").val(),
+                address : $("#address").val(),
+                nationality : $("#nationality").val(),
+                pps :  $("#pps").val()
+            })
+        });
+
+    });
+});
